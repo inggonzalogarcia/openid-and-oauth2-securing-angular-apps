48f743cc0b9d7e2e28cb014b41ff778cea8e54d1
Gonzalo Garcia <ing.gonzalogarcia@outlook.com>
2022-03-28 13:53:22 -0300

    4.Connecting to a Different OpenID Connect Provider, Auth0

--- securing-angular-client-ng11/src/app/constants.ts
+++ securing-angular-client-ng11/src/app/constants.ts
@@ -2,10 +2,12 @@ export class Constants {
 	public static clientRoot = 'http://localhost:4200/';
 
 	public static apiRoot = 'https://securingangularappscoursev2-api-unsecure.azurewebsites.net/api/';
-	public static stsAuthority = 'https://securingangularappscoursev2-sts.azurewebsites.net/';
+	// public static stsAuthority = 'https://securingangularappscoursev2-sts.azurewebsites.net/';
 
 	// public static apiRoot = 'https://localhost:2112/api/';
 	// public static stsAuthority = 'https://localhost:4242/';
+	public static stsAuthority = 'https://dev-2tzw78v0.us.auth0.com/';
 
-	public static clientId = 'spa-client';
+	//public static clientId = 'spa-client';
+	public static clientId = 'eCBMXiJ9PDoTPKVvINaMl2kZg2Ctik0M';
 }
