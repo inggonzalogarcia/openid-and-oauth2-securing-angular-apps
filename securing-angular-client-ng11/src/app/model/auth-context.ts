afa878b03affbb0ad73c522388684fec72f9d619
Gonzalo Garcia <ing.gonzalogarcia@outlook.com>
2022-03-29 17:29:01 -0300

    6.Managing User Experience Based on the Security Context

--- securing-angular-client-ng11/src/app/model/auth-context.ts
+++ securing-angular-client-ng11/src/app/model/auth-context.ts
@@ -4,4 +4,8 @@ import { SimpleClaim } from './simple-claim';
 export class AuthContext {
   claims: SimpleClaim[];
   userProfile: UserProfile;
+
+  get isAdmin() {
+    return !!this.claims && !!this.claims.find(c => c.type === 'role' && c.value === 'Admin');
+  }
 }
