fd926bfba35b372c6db3dd79bc2544c8333cf608
Gonzalo Garcia <ing.gonzalogarcia@outlook.com>
2022-03-29 13:28:57 -0300

    5.OAuth2.Handling Authorization Errors in the Client App

--- securing-angular-client-ng11/src/app/core/auth-interceptor.service.ts
+++ securing-angular-client-ng11/src/app/core/auth-interceptor.service.ts
@@ -1,26 +1,31 @@
+import { HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 import { from, Observable } from 'rxjs';
-import { CoreModule } from './core.module';
-import { AuthService } from './auth-service.component';
+import { tap } from 'rxjs/operators';
 import { Constants } from '../constants';
+import { AuthService } from './auth-service.component';
 
 @Injectable()
 export class AuthInterceptorService implements HttpInterceptor {
-    constructor(private _authService: AuthService) {}
+  constructor(private _authService: AuthService,
+    private _router: Router) { }
 
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        if (req.url.startsWith(Constants.apiRoot)) {
-            return from(this._authService.getAccessToken().then(token => {
-                const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-                const authReq = req.clone({ headers });
-                return next.handle(authReq).toPromise();
-            }));
-        }
-        else {
-            return next.handle(req);
-        }
-        
-        
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (req.url.startsWith(Constants.apiRoot)) {
+      return from(this._authService.getAccessToken().then(token => {
+        const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+        const authReq = req.clone({ headers });
+        return next.handle(authReq).pipe(tap(_ => { }, error => {
+          var respError = error as HttpErrorResponse;
+          if (respError && (respError.status === 401 || respError.status === 403)) {
+            this._router.navigate(['/unauthorized']);
+          }
+        })).toPromise();
+      }));
     }
+    else {
+      return next.handle(req);
+    }
+  }
 }
